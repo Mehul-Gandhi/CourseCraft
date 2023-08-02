7351bbcb663fe7c7f4e14131ca9cda2c02fac896
Mehul-Gandhi <gandhi@berkeley.edu>
2023-08-02 14:03:01 -0700

    Change class component to functional component

--- my-app/src/components/UploadButton.js
+++ my-app/src/components/UploadButton.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/UploadButton.css'; // assuming you have a css file named UploadButton.css
 
-class UploadButton extends Component {
-  handleFileUpload = (event) => {
+export default function UploadButton () {
+  const handleFileUpload = (event) => {
     const file = event.target.files[0];
     console.log('Selected file:', file);
     // Implement file upload logic here
   };
 
-  render() {
-    return (
-      <div className="upload-btn-wrapper">
-        <button className="btn">Upload</button>
-        <input type="file" name="myfile" onChange={this.handleFileUpload} />
-      </div>
-    );
-  }
-}
-
-export default UploadButton;
+  return (
+    <div className="upload-btn-wrapper">
+      <button className="btn">Upload</button>
+      <input type="file" name="myfile" onChange={handleFileUpload} />
+    </div>
+  );
+};
