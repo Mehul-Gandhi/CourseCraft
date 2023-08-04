feb681acdb150f6af281e797d91d4823db0b4191
Mehul-Gandhi <gandhi@berkeley.edu>
2023-08-03 18:55:06 -0700

    Finish navbar rough draft, still need to edit it

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -4,13 +4,9 @@ import "../index.css";
 
 export default function NavBar() {
     return (
-        <nav className="bg-[#032054] py-6 px-8">
-             <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It's easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-            <div className="container mx-auto flex justify-between items-center flex-col md:flex-row">
+        <nav className="bg-[#032054]" style={{"backgroundColor": "#003262"}}>
+            <div style={{backgroundColor: '#FFB81C', height: '20px', width: '100%'}}></div>
+            <div className="mx-auto flex justify-end items-center flex-col md:flex-row" >
                 <ul className="flex space-x-4">
                     <li>
                         <Link to="/schedule" className="text-white hover:bg-red-700 px-3 py-2 rounded-md text-sm font-medium">
@@ -23,7 +19,7 @@ export default function NavBar() {
                         </Link>
                     </li>
                     <li>
-                    <Link to="/calendar" className="text-white hover:bg-red-700 px-3 py-2 rounded-md text-sm font-medium">
+                        <Link to="/calendar" className="text-white hover:bg-red-700 px-3 py-2 rounded-md text-sm font-medium">
                             GCal & Tasks
                         </Link>
                     </li>
