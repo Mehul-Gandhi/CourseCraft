dd241c38e626acd5b1e78c68e4c578c44cdf950e
unknown <lib_laptop@MOFF-ASUC-107.lib.berkeley.edu>
2023-08-07 16:53:38 -0700

    timeline

--- my-app/src/components/TaskDoc.js
+++ my-app/src/components/TaskDoc.js
@@ -10,4 +10,4 @@ function TaskDoc() {
   }
   
   export default TaskDoc;
-  
+  */
--- my-app/src/components/TimeLine.js
+++ my-app/src/components/TimeLine.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
@@ -7,7 +8,6 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/system";
 
-// Custom styles for color and connector
 const ColorStepIcon = styled('span')(({ theme }) => ({
   '&.MuiStepIcon-root': {
     color: '#FFB81C'
@@ -20,67 +20,116 @@ const ColorStepIcon = styled('span')(({ theme }) => ({
   }
 }));
 
+const CustomStepIconContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column', 
+  alignItems: 'center',
+}));
+
+const CustomStepIcon = styled('div')(({ theme, active }) => ({
+  width: 40,
+  height: 40,
+  borderRadius: '50%', 
+  backgroundColor: active ? '#FFB81C' : '#ffffff', 
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: active ? '#ffffff' : '#000000', 
+  fontSize: 18,
+  marginBottom: 8, 
+  border: `2px solid ${active ? '#FFB81C' : '#000000'}`, 
+}));
+
 const CustomConnector = styled('div')(({ theme }) => ({
   flex: '1 1 auto',
   marginTop: 8,
-  borderBottom: '1px solid #FFB81C',
+  display: 'flex',
+  flexDirection: 'column', 
+  alignItems: 'center', 
 }));
 
 function TimeLine() {
-    const [activeStep, setActiveStep] = React.useState(0);
+  const [activeStep, setActiveStep] = React.useState(0);
 
-    const nextStep = () => {
-        if (activeStep < 4) 
-            setActiveStep((currentStep) => currentStep + 1);
-    }
+  const handleNextStep = () => {
+    if (activeStep < 4)
+      setActiveStep((currentStep) => currentStep + 1);
+  }
 
-    const previousStep = () => {
-        if (activeStep !== 0)
-            setActiveStep((currentStep) => currentStep - 1);
-    }
+  const handlePreviousStep = () => {
+    if (activeStep !== 0)
+      setActiveStep((currentStep) => currentStep - 1);
+  }
 
-    return (
-        <Container maxWidth="md" style={{ display: 'flex', justifyContent: 'center' }}>
-            <Box width="50%">
-                <Stepper activeStep={activeStep} connector={<CustomConnector />}>
-                    <Step>
-                        <StepLabel StepIconComponent={ColorStepIcon}>first</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel StepIconComponent={ColorStepIcon}>second</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel StepIconComponent={ColorStepIcon}>third</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel StepIconComponent={ColorStepIcon}>fourth</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel StepIconComponent={ColorStepIcon}>fifth</StepLabel>
-                    </Step>
-                </Stepper>
-                <h3>{activeStep}</h3>
-                <Button 
-                    variant="outlined" 
-                    color="primary"
-                    onClick={nextStep}
-                    fullWidth
-                >
-                    Next step
-                </Button>
-                <br />
-                <br />
-                <Button 
-                    variant="outlined" 
-                    color="primary"
-                    onClick={previousStep}
-                    fullWidth
-                >
-                    Previous step
-                </Button>
-            </Box>
-        </Container>
-    );
+  return (
+    <Container maxWidth="md" style={{ display: 'flex', justifyContent: 'center' }}>
+      <Box width="50%">
+        <Stepper activeStep={activeStep} connector={<CustomConnector />}>
+          <Step>
+            <StepLabel StepIconComponent={CustomStepIconContainer}>
+              <CustomStepIcon active={activeStep === 0}>
+                1
+              </CustomStepIcon>
+              Upload current schedule
+            </StepLabel>
+          </Step>
+          <Step>
+            <StepLabel StepIconComponent={CustomStepIconContainer}>
+              <CustomStepIcon active={activeStep === 1}>
+                2
+              </CustomStepIcon>
+              Generate new schedule
+            </StepLabel>
+          </Step>
+          <Step>
+            <StepLabel StepIconComponent={CustomStepIconContainer}>
+              <CustomStepIcon active={activeStep === 2}>
+                3
+              </CustomStepIcon>
+              Confirm new schedule
+            </StepLabel>
+          </Step>
+          <Step>
+            <StepLabel StepIconComponent={CustomStepIconContainer}>
+              <CustomStepIcon active={activeStep === 3}>
+                4
+              </CustomStepIcon>
+              Generate website code
+            </StepLabel>
+          </Step>
+          <Step>
+            <StepLabel StepIconComponent={CustomStepIconContainer}>
+              <CustomStepIcon active={activeStep === 4}>
+                5
+              </CustomStepIcon>
+              Add to GCal & tasks
+            </StepLabel>
+          </Step>
+        </Stepper>
+        {/*  <h3>{activeStep}</h3> */}
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={handleNextStep}
+          fullWidth
+          disabled={activeStep === 4}
+        >
+          Next step
+        </Button>
+        <br />
+        <br />
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={handlePreviousStep}
+          fullWidth
+          disabled={activeStep === 0}
+        >
+          Previous step
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
 export default TimeLine;
