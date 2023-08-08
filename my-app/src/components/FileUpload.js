424e837d77980d54493c6358d0a5bf191b6395fd
Manan Bhargava <manan.bhargava@berkeley.edu>
2023-08-07 23:06:00 -0700

    added the file upload component

--- my-app/src/components/FileUpload.js
+++ my-app/src/components/FileUpload.js
@@ -1,89 +1,111 @@
-// import React from 'react'
-// import '../styles/FileUpload.css'
+import React, { useRef, useState } from 'react';
+import '../styles/FileUpload.css'; // Assuming you have a CSS file for styles
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudUploadAlt, faFileAlt, faCheck } from '@fortawesome/free-solid-svg-icons';
 
-// function FileUpload() {
-//   return (
-//     <div>
-//       <header>File Uploader JavaScript</header>
-//       <form action="#">
-//         <input class="file-input" type="file" name="file" hidden></input>
-//         <i class="fas fa-cloud-upload-alt"></i>
-//         <p>Browse File to Upload</p>
-//       </form>
-//       <section class="progress-area"></section>
-//       <section class="uploaded-area"></section>
-//     </div>
-//   )
-// }
+function FileUpload() {
+  const fileInput = useRef(null);
+  const [uploadData, setUploadData] = useState({});
 
-// // Adding the form
-// const form = document.querySelector("form"),
-// fileInput = document.querySelector(".file-input"),
-// progressArea = document.querySelector(".progress-area"),
-// uploadedArea = document.querySelector(".uploaded-area");
+  const handleClick = () => {
+    fileInput.current.click();
+  }
 
-// // form click event
-// form.addEventListener("click", () =>{
-//   fileInput.click();
-// });
+  const handleChange = (event) => {
+    var file = event.target.files[0];
+    // If the file exists
+    if (file) {
+      var fileName = file.name;
+      if (fileName.length >= 12) {
+        var splitName = fileName.split('.');
+        fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+      }
+      uploadFile(file, fileName);
+    }
+  }
 
-// // When a file is uploaded
-// fileInput.onchange = ({target})=>{
-//   let file = target.files[0]; //getting file [0] this means if user has selected multiple files then get first one only
-//   if (file){
-//     let fileName = file.name; //getting file name
-//     if (fileName.length >= 12){ //if file name length is greater than 12 then split it and add ...
-//       let splitName = fileName.split('.');
-//       fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
-//     }
-//     uploadFile(fileName); //calling uploadFile with passing file name as an argument
-//   }
-// }
+  const uploadFile = (file, name) => {
+    var formData = new FormData();
+    formData.append('file', file, name);
 
-// // file upload function
-// function uploadFile(name){
-//   let xhr = new XMLHttpRequest(); //creating new xhr object (AJAX)
-//   xhr.open("POST", "php/upload.php"); //sending post request to the specified URL
-//   xhr.upload.addEventListener("progress", ({loaded, total}) =>{ //file uploading progress event
-//     let fileLoaded = Math.floor((loaded / total) * 100);  //getting percentage of loaded file size
-//     let fileTotal = Math.floor(total / 1000); //gettting total file size in KB from bytes
-//     let fileSize;
-//     // if file size is less than 1024 then add only KB else convert this KB into MB
-//     (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
-//     let progressHTML = `<li class="row">
-//                           <i class="fas fa-file-alt"></i>
-//                           <div class="content">
-//                             <div class="details">
-//                               <span class="name">${name} • Uploading</span>
-//                               <span class="percent">${fileLoaded}%</span>
-//                             </div>
-//                             <div class="progress-bar">
-//                               <div class="progress" style="width: ${fileLoaded}%"></div>
-//                             </div>
-//                           </div>
-//                         </li>`;
-//     // uploadedArea.innerHTML = ""; //uncomment this line if you don't want to show upload history
-//     uploadedArea.classList.add("onprogress");
-//     progressArea.innerHTML = progressHTML;
-//     if (loaded === total){
-//       progressArea.innerHTML = "";
-//       let uploadedHTML = `<li class="row">
-//                             <div class="content upload">
-//                               <i class="fas fa-file-alt"></i>
-//                               <div class="details">
-//                                 <span class="name">${name} • Uploaded</span>
-//                                 <span class="size">${fileSize}</span>
-//                               </div>
-//                             </div>
-//                             <i class="fas fa-check"></i>
-//                           </li>`;
-//       uploadedArea.classList.remove("onprogress");
-//       // uploadedArea.innerHTML = uploadedHTML; //uncomment this line if you don't want to show upload history
-//       uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML); // remove this line if you don't want to show upload history
-//     }
-//   });
-//   let data = new FormData(form); //FormData is an object to easily send form data
-//   xhr.send(data); //sending form data
-// }
+    // To display the progress upload bar
+    let xhr = new XMLHttpRequest();
+  
+    xhr.open('POST', 'php/upload.php', true);
+  
+    // Event listener for the progress event
+    xhr.upload.onprogress = function(event) {
+      if (event.lengthComputable) {
+        let percentComplete = Math.round((event.loaded / event.total) * 100);
+        let fileTotal = Math.floor(event.total / 1000);
+        let fileSize = fileTotal < 1024 ? fileTotal + " KB" : (event.loaded / (1024*1024)).toFixed(2) + " MB";
+        
+        // Update the state with the progress
+        setUploadData(prevData => ({
+          ...prevData,
+          [name]: { progress: percentComplete, fileSize, status: 'Uploading' }
+        }));
+      }
+    };
+  
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        setUploadData(prevData => ({
+          ...prevData,
+          [name]: { ...prevData[name], status: 'Uploaded' }
+        }));
+      } else {
+        // Handle the error
+      }
+    };
+  
+    xhr.send(formData);
+  }
 
-// export default FileUpload;
+  return (
+    <div className="wrapper">
+      <header>Upload Files Here</header>
+
+      {/* Handle click area */}
+      <form onClick={handleClick}>
+        <input ref={fileInput} className="file-input" type="file" name="file" hidden onChange={handleChange} />
+        <FontAwesomeIcon icon={faCloudUploadAlt} />
+        <p>Browse File to Upload</p>
+      </form>
+
+      {/* Dynamic file visualizer section */}
+      <section className="progress-area">
+        {Object.keys(uploadData).map(name => (
+          <div key={name} className="row">
+            <FontAwesomeIcon icon={faFileAlt} />
+            <div className="content">
+              <div className="details">
+                <span className="name">{name} • {uploadData[name].status}</span>
+                <span className="percent">{uploadData[name].progress}%</span>
+              </div>
+              <div className="progress-bar">
+                <div className="progress" style={{width: `${uploadData[name].progress}%`}}></div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+      <section className="uploaded-area">
+        {Object.keys(uploadData).filter(name => uploadData[name].status === 'Uploaded').map(name => (
+          <div key={name} className="row">
+            <div className="content upload">
+              <FontAwesomeIcon icon={faFileAlt} />
+              <div className="details">
+                <span className="name">{name} • Uploaded</span>
+                <span className="size">{uploadData[name].fileSize}</span>
+              </div>
+            </div>
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default FileUpload;
