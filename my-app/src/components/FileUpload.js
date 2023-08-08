18325bfff774f3928c51cbbd72cabe374ca228a3
Mehul-Gandhi <gandhi@berkeley.edu>
2023-08-08 12:08:43 -0700

    Bug in uploadFile

--- my-app/src/components/FileUpload.js
+++ my-app/src/components/FileUpload.js
@@ -3,75 +3,97 @@ import '../styles/FileUpload.css'; // Assuming you have a CSS file for styles
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCloudUploadAlt, faFileAlt, faCheck } from '@fortawesome/free-solid-svg-icons';
 
-function FileUpload() {
+function FileUpload({ uploadData, setUploadData }) {
   const fileInput = useRef(null);
-  const [uploadData, setUploadData] = useState({});
 
   const handleClick = () => {
     fileInput.current.click();
   }
 
-  const handleChange = (event) => {
+  const handleChange = async (event) => {
+    if (uploadData.length >= 10) {
+      alert("You can upload a maximum of 10 files.");
+      return;
+    }
     var file = event.target.files[0];
-    // If the file exists
+    // Convert to base64
+    const base64 = await convertToBase64(file);
+
     if (file) {
       var fileName = file.name;
       if (fileName.length >= 12) {
         var splitName = fileName.split('.');
         fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
       }
+
+      // Append the base64 representation of the file to uploadData state
+      setUploadData(prevData => [...prevData, {
+        name: fileName,
+        base64: base64,
+        progress: 0,
+        fileSize: "0 KB",
+        status: 'Pending'
+      }]);
+      
       uploadFile(file, fileName);
     }
-    console.log(uploadData);
-  }
-  function convertToBase64(e) {
-    console.log(e);
-    var reader = new FileReader();
-    reader.readAsDataURL(e.target.files[0]);
-    reader.onload = () => {
-      console.log(reader.result);
-    }
-    reader.onerror = error => {
-      console.log("Error: ", error);
-    }
   };
+
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  }
+
+
   const uploadFile = (file, name) => {
     var formData = new FormData();
     formData.append('file', file, name);
 
-    // To display the progress upload bar
     let xhr = new XMLHttpRequest();
-  
     xhr.open('POST', 'php/upload.php', true);
-  
-    // Event listener for the progress event
+
     xhr.upload.onprogress = function(event) {
       if (event.lengthComputable) {
         let percentComplete = Math.round((event.loaded / event.total) * 100);
         let fileTotal = Math.floor(event.total / 1000);
         let fileSize = fileTotal < 1024 ? fileTotal + " KB" : (event.loaded / (1024*1024)).toFixed(2) + " MB";
-        
-        // Update the state with the progress
-        setUploadData(prevData => ({
-          ...prevData,
-          [name]: { progress: percentComplete, fileSize, status: 'Uploading' }
-        }));
+
+        setUploadData(prevData => {
+          const fileIndex = prevData.findIndex(file => file.name === name);
+          if (fileIndex !== -1) {
+            const newFile = {...prevData[fileIndex], progress: percentComplete, fileSize, status: 'Uploading'};
+            return [
+              ...prevData.slice(0, fileIndex),
+              newFile,
+              ...prevData.slice(fileIndex + 1)
+            ];
+          }
+          return prevData;
+        });
       }
     };
 
-  
-  
     xhr.onload = function() {
       if (xhr.status === 200) {
-        setUploadData(prevData => ({
-          ...prevData,
-          [name]: { ...prevData[name], status: 'Uploaded' }
-        }));
-      } else {
-        // Handle the error
+        setUploadData(prevData => {
+          const fileIndex = prevData.findIndex(file => file.name === name);
+          if (fileIndex !== -1) {
+            const newFile = {...prevData[fileIndex], status: 'Uploaded'};
+            return [
+              ...prevData.slice(0, fileIndex),
+              newFile,
+              ...prevData.slice(fileIndex + 1)
+            ];
+          }
+          return prevData;
+        });
       }
     };
-  
+
     xhr.send(formData);
   }
 
@@ -81,7 +103,7 @@ function FileUpload() {
 
       {/* Handle click area */}
       <form onClick={handleClick}>
-        <input ref={fileInput} className="file-input" type="file" name="file" hidden onChange={convertToBase64} />
+        <input ref={fileInput} className="file-input" type="file" name="file" hidden onChange={handleChange} />
         <FontAwesomeIcon icon={faCloudUploadAlt} />
         <p>Browse File to Upload</p>
       </form>
