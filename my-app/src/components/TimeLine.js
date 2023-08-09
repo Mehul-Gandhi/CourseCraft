0b695f69e710de36bbc6874e20c41e84874000a8
Mehul-Gandhi <gandhi@berkeley.edu>
2023-08-09 16:16:22 -0700

    Finish timeline

--- my-app/src/components/TimeLine.js
+++ my-app/src/components/TimeLine.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import '../styles/TimeLine.css';
+import CheckIcon from '@mui/icons-material/Check';
 
-function TimeLine() {
-    const items = ['Bacon', 'Rib', 'Sausage'];
+function TimeLine({ page }) {
+    const items = ['Upload Files', 'Generate New Schedule', 'Confirm New Schedule'];
 
     return (
         <div className="container" >
             <ul className="timeline">
                 {items.map((item, index) => (
                     <li 
-                        className={`timeline-item ${index === 0 ? "active" : ""} hello`} 
+                        className={`timeline-item ${index < page ? "active" : ""} hello`} 
                         key={index}
                     >
-                        <div className="circle">{index + 1}</div>
+                        <div className="circle">{index < page?  <CheckIcon />: "" }</div>
                         {item}
                     </li>
                 ))}
