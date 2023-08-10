30eac1e8fdebfd96e59ae166f31ebecddbdd26fe
Mehul-Gandhi <gandhi@berkeley.edu>
2023-08-10 10:22:39 -0700

    Screwed up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Upload from "./components/Upload";
 import NavBar from "./components/NavBar";
@@ -9,23 +9,35 @@ import LandingPage from "./components/LandingPage";
 import NotFound from "./components/NotFound"; // Don't forget to import this too.
 import Calendar from "./components/Calendar";
 
-function App() {
-     
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
 
-    return (
-      <Router>
-        {/* <NavBar /> */}
-        <Routes>
-          <Route index element={<LandingPage />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/help" element={<Instructions />} />
-          <Route path="/schedule" element={<CompareSchedule />} />
-          <Route path="/shared" element= {<SharedSchedule />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-          <Route path="/calendar" element={<Calendar />} /> 
-        </Routes>
-      </Router>
-    )
-}
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
+  return (
+    <Toast show={show} onClose={() => toggleShow(!show)}>
+      <Toast.Header>
+        <strong className="mr-auto">React-Bootstrap</strong>
+      </Toast.Header>
+      <Toast.Body>{children}</Toast.Body>
+    </Toast>
+  );
+};
+
+
+const App = () => (
+  <Container className="p-3">
+    <Container className="p-5 mb-4 bg-light rounded-3">
+      <h1 className="header">Welcome To React-Bootstrap</h1>
+      <ExampleToast>
+        We now have Toasts
+        <span role="img" aria-label="tada">
+          🎉
+        </span>
+      </ExampleToast>
+    </Container>
+  </Container>
+);
 
 export default App;
