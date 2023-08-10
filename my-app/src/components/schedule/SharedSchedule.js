4fbec51aeddc98485e08e9d063b082e60a340832
Mehul-Gandhi <gandhi@berkeley.edu>
2023-08-10 00:25:05 -0700

    Copy key works now

--- my-app/src/components/schedule/SharedSchedule.js
+++ my-app/src/components/schedule/SharedSchedule.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Banner from '../Banner';
 import LoginButton from '../login/LoginButton';
 import LogoutButton from '../login/LogoutButton';
 import LinkIcon from '@mui/icons-material/Link';
 import Button from '../buttons/Button';
 import placeholder from '../../assets/placeholder.png'; 
-import CodeEditor from '../website/AceEditor'; // Ensure you import the CodeEditor component
+import CodeEditor from '../website/AceEditor';
 import CheckIcon from '@mui/icons-material/Check';
 
 import DownloadIcon from '@mui/icons-material/Download';
@@ -19,15 +19,16 @@ import "../../index.css";
 
 function SharedSchedule() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showEditor, setShowEditor] = useState(false); // New state variable
+  const [showEditor, setShowEditor] = useState(false);
   const [displayedText, setDisplayedText] = useState("");
   const [startTyping, setStartTyping] = useState(true);
-  const [copyCode, setCopyCode] = useState(false)
+  const [copySuccess, setCopySuccess] = useState('');  // New state for copy success message
   
   const location = useLocation();
   const navigate = useNavigate();
-
+  
   var { key, classWebsite, courseWebsite, uploadData, department, semester, year } = location.state;
+
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
@@ -36,11 +37,8 @@ function SharedSchedule() {
   const amOrPm = hours >= 12 ? 'pm' : 'am';
 
   const time = `${formattedHours}:${formattedMinutes}${amOrPm}`;
-  const textareaRef = useRef(null);
+  const fullText = `${department || "CS10" } ${semester || "Spring"} ${year || "2024"} table generated at ${time} ${classWebsite} ${courseWebsite}`;
 
-  const fullText = `${department || "CS10" } ${semester || "Spring"} ${year || "2024"} table generated at ${time}\
-  ${classWebsite} ${courseWebsite}`;
-  //{ key, classWebsite, courseWebsite, uploadData, department, semester, year }
   useEffect(() => {
     let i = 0;
     if (startTyping && i < fullText.length) {
@@ -56,42 +54,34 @@ function SharedSchedule() {
     }
   }, [startTyping]);
 
-
-
-
-  // console.log(uploadData);
-
-  const [language, setLanguage] = useState("javascript"); // default language
-
+  const [language, setLanguage] = useState("javascript");
   const handleLanguageChange = (event) => {
     setLanguage(event.target.value);
   }
+  
   const generateCalendar = () => {
     navigate("/calendar", { state: { uploadData, department, semester, year } });
   }
 
-  const handleClick = () => {
-    console.log('Button clicked');
-  };
-  
   const handleEditorToggle = () => {
     setShowEditor(true);
   };
-  
+
   const navigateBack = () => {
-    window.history.back()
+    window.history.back();
   }
-  function handleCopy() {
-    const keyToCopy = location.state.key; // Get the key value from the location state
-    const tempTextarea = document.createElement('textarea');
-    tempTextarea.value = keyToCopy;
-    document.body.appendChild(tempTextarea);
-    tempTextarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(tempTextarea);
-    setCopyCode(true);
-  }
-  
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(key)
+      .then(() => {
+        setCopySuccess('Key Copied!');
+      })
+      .catch(err => {
+        console.error('Failed to copy text: ', err);
+        setCopySuccess('Failed to copy key!');
+      });
+  };
+
   return (
     <div className="App">
       {isLoggedIn ? (
@@ -107,17 +97,13 @@ function SharedSchedule() {
       <Button onClick={navigateBack} icon={<ArrowBackIcon />} text={"Back"}/>
       <Banner text={""}/>
       <div className="font-bold text-white text-md">
-        <h1> <LinkIcon /> Shared key: {key}
-        </h1>
-        <button 
-    onClick={handleCopy} 
-    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
-  >
-    {copyCode ? <> <CheckIcon /> Copied </> : "Copy Code"}
-</button>
-
-        </div>
-
+        <h1><LinkIcon /> Shared key: {key}</h1>
+        <button onClick={handleCopy} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
+          Copy Key
+        </button>
+        {copySuccess && <div style={{color: 'green'}}>{copySuccess}</div>}
+      </div>
+      
       <div className="font-bold text-white text-md">{displayedText}</div>
       
       {showEditor && <select value={language} onChange={handleLanguageChange}>
@@ -126,11 +112,11 @@ function SharedSchedule() {
         <option value="html">HTML</option>
         <option value="markdown">Markdown</option>
       </select>}
-
+      
       {showEditor ? <CodeEditor language={language} /> : <img src={placeholder} alt="Placeholder 2024" className="mx-auto md:w-75 h-auto"/>}
 
       <div className="flex justify-center items-center space-x-5" style={{padding: "25px"}}>
-        <Button onClick={handleClick} icon={<DownloadIcon />} text={"Download master calendar .ics"}/>
+        <Button icon={<DownloadIcon />} text={"Download master calendar .ics"}/>
         <Button onClick={generateCalendar} icon={<GoogleIcon />} text={"Google Calendar & Role-based Google Tasks"}/>
         <Button onClick={handleEditorToggle} icon={<CodeIcon />} text={"Website Code"}/>
       </div>
